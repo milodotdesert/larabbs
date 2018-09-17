105134e706b9901c99ad8237a14121e066467046
milo.desert <milo.desert@qq.com>
2018-09-17 20:34:22 +0800

    5.3

--- routes/web.php
+++ routes/web.php
@@ -37,4 +37,6 @@ Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
  */
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
+
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
