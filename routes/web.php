3122dcbb6f582d15ddf18122412f8a2b9fb80708
milo.desert <milo.desert@qq.com>
2018-09-28 21:10:54 +0800

    6.1

--- routes/web.php
+++ routes/web.php
@@ -42,3 +42,4 @@ Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
 Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
+Route::resource('replies', 'RepliesController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
